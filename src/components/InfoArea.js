2faabea3fd56859b4707d18a694cddde36a2dac2
FrankBlack <fonyeabo12@gmail.com>
2020-08-26 21:49:04 -0500

    files added

---
+++ src/components/InfoArea.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import DataTable from './DataTable';
+import Nav from './Nav';
+import API from './assets/API';
+import InfoAreaContext from './assets/InfoAreaContext';
+
+
+const InfoArea = () => {
+    const [present, futureState] = useState({
+        users: [],
+        order: 'descend',
+        filteredUsers: [],
+        headings: [
+            {name: "Image", width: "20%"},
+            {name: "Name", width: "20%"},
+            {name: "Email", width: "20%"},
+            {name: "Tel", width: "10%"}
+        ]
+    })
+};
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SeachBae=r';
+
+function Nav () {
+    return (
+        <nav className="navbar navbar-light bg-light">
+        <div className="search-area col-4">
+            <SearchBar />
+  </div>
+</nav>
+    );
+}
+
+export default Nav;
