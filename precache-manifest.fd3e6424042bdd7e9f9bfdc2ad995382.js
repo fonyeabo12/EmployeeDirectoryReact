57821cbcf2b0aee7f0bc96d1bfdaa8ee87ea9d0b
FrankBlack <fonyeabo12@gmail.com>
2020-08-26 22:10:22 -0500

    Updates

---
+++ precache-manifest.fd3e6424042bdd7e9f9bfdc2ad995382.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b757d15387b17ac6cbe2",
+    "url": "/static/css/main.36d12178.chunk.css"
+  },
+  {
+    "revision": "b757d15387b17ac6cbe2",
+    "url": "/static/js/main.aac134e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1868546b76755d13629b",
+    "url": "/static/js/2.cdefec29.chunk.js"
+  },
+  {
+    "revision": "ac5ecfa6bc2930c90b78273f20bf18c5",
+    "url": "/index.html"
+  }
+];
